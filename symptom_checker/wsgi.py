14b08b7ce5c18d1d22fdd80487c8349327c9bbc1
priyaa2468 <priyakalappagol4@gmail.com>
2026-06-18 22:20:04 +0530

    Fix missing newline at end of wsgi.py
    Added a newline at the end of the file.

--- symptom_checker/wsgi.py
+++ symptom_checker/wsgi.py
@@ -1,4 +1,6 @@
-import os
-from django.core.wsgi import get_wsgi_application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'symptom_checker.settings')
-application = get_wsgi_application()
++ import os
++ from django.core.wsgi import get_wsgi_application
+
++ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'symptom_checker.settings')
+
++ application = get_wsgi_application()
