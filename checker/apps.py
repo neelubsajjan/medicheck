8885f286adb3081b610291dfac353c5f2d0d39be
nanditha7777 <Nandithav35@gmail.com>
2026-06-18 22:42:44 +0530

    Update apps.py

--- checker/apps.py
+++ checker/apps.py
@@ -1,5 +1,12 @@
 from django.apps import AppConfig
+import logging
+
+logger = logging.getLogger(__name__)
 
 class CheckerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'checker'
+    verbose_name = 'AI Disease Recognizer'
+
+    def ready(self):
+        logger.info("AI Disease Recognizer v2.0 Loaded Successfully")
